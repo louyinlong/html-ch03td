44274c84689ebdee5a1cf5dbcd11ed11200b7bed
louyinlong <1092755001@qq.com>
2020-04-19 19:47:10 +0800

    ch03td
    Signed-off-by: louyinlong <1092755001@qq.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MENUS, PRODUCTS } from './data';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'td';
+  menus = MENUS;
+
+  products = PRODUCTS;
 }
